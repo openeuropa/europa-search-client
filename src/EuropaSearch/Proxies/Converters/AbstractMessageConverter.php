b27815bba721a8d7967fb63acf9803735f61bb43
Gilles Deudon <gilles.deudon@gmail.com>
2017-12-07 08:24:49 +0100

    NEPT-940: Remove the APIVersion version in the Proxies layer as it is identified as useless.

--- src/EuropaSearch/Proxies/Converters/AbstractMessageConverter.php
+++ src/EuropaSearch/Proxies/Converters/AbstractMessageConverter.php
@@ -40,15 +40,6 @@ abstract class AbstractMessageConverter implements MessageConverterInterface
         try {
             $rawResult = \GuzzleHttp\json_decode($response->getBody()->getContents());
 
-            if (!isset($rawResult->apiVersion)) {
-                throw new ProxyException("The api version has not been communicated by the web service");
-            }
-
-            $supportedAPI = $configuration->getSupportedServiceAPIVersion();
-            if (!Semver::satisfies($rawResult->apiVersion, $supportedAPI)) {
-                throw new ProxyException("The service api version is incompatible with the client's supported one: ".$supportedAPI);
-            }
-
             return $rawResult;
         } catch (\InvalidArgumentException $iae) {
             throw new ProxyException("The service response is not recognized by the client", $iae);
