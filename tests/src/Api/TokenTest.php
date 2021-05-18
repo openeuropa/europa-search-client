e3c8d993a0c1ff5fd1fab334093c40911626bc7e
Claudiu Cristea <clau.cristea@gmail.com>
2021-05-18 15:06:43 +0300

    OEL-100: Make token test extendable.

--- tests/src/Api/TokenTest.php
+++ tests/src/Api/TokenTest.php
@@ -19,28 +19,45 @@ class TokenTest extends TestCase
 
     /**
      * @covers ::getToken
+     * @dataProvider providerTestToken
+     *
+     * @param array $clientConfig
+     * @param mixed $response
+     * @param mixed $expectedResult
      */
-    public function testSearch(): void
+    public function testToken(array $clientConfig, $response, $expectedResult): void
     {
-        $client = $this->getTestingClient(
-            [
-                'tokenApiEndpoint' => 'http://example.com/token',
-                'consumerKey' => 'foo',
-                'consumerSecret' => 'bar',
-            ],
-            [
-                new Response(200, [], '{"access_token":"jwt_token","scope":"application_scope","token_type":"Bearer","expires_in":3600}'),
-            ]
-        );
-
+        $client = $this->getTestingClient($clientConfig, [$response]);
         /** @var TokenInterface $tokenService */
         $tokenService = $client->getContainer()->get('token');
-        $result = $tokenService->getToken();
+        $actualResult = $tokenService->getToken();
+        $this->assertEquals($expectedResult, $actualResult);
+    }
 
-        $this->assertInstanceOf(TokenResult::class, $result);
-        $this->assertSame('jwt_token', $result->getAccessToken());
-        $this->assertSame('application_scope', $result->getScope());
-        $this->assertSame('Bearer', $result->getTokenType());
-        $this->assertSame(3600, $result->getExpiresIn());
+    /**
+     * @return array
+     */
+    public function providerTestToken(): array
+    {
+        return [
+            'simple token call' => [
+                [
+                    'tokenApiEndpoint' => 'http://example.com/token',
+                    'consumerKey' => 'foo',
+                    'consumerSecret' => 'bar',
+                ],
+                new Response(200, [], json_encode([
+                    'access_token' => 'JWT_TOKEN',
+                    'scope' => 'APPLICATION_SCOPE',
+                    'token_type' => 'Bearer',
+                    'expires_in' => 3600,
+                ])),
+                (new TokenResult())
+                    ->setAccessToken('JWT_TOKEN')
+                    ->setScope('APPLICATION_SCOPE')
+                    ->setTokenType('Bearer')
+                    ->setExpiresIn(3600),
+            ],
+        ];
     }
 }
