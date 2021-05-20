01b0d4401ca59a8c2ae7339a7ffabdf70ccf9096
Claudiu Cristea <clau.cristea@gmail.com>
2021-05-20 21:57:19 +0300

    OEL-97: Add a FacetInterface.

---
+++ src/Contract/FacetInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEuropa\EuropaSearchClient\Contract;
+
+use OpenEuropa\EuropaSearchClient\Model\FacetValue;
+
+interface FacetInterface extends SearchBaseInterface
+{
+    /**
+     * @return FacetValue[]
+     */
+    public function getFacets(): array;
+
+    /**
+     * @param string|null $displayLanguage
+     * @todo Validate passed language code against ISO-639-1 in OEL-154.
+     */
+    public function setDisplayLanguage(?string $displayLanguage): self;
+
+    /**
+     * @return string|null
+     */
+    public function getDisplayLanguage(): ?string;
+}
