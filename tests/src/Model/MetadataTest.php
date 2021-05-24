9dc723c713aa42d1878015fb11ab43ff1541ca46
Claudiu Cristea <clau.cristea@gmail.com>
2021-05-24 11:36:51 +0300

    OEL-115: Fix tests QA remarks.

--- tests/src/Model/MetadataTest.php
+++ tests/src/Model/MetadataTest.php
@@ -8,43 +8,89 @@ use OpenEuropa\EuropaSearchClient\Model\Metadata;
 use PHPUnit\Framework\TestCase;
 
 /**
- * Tests the Metadata model class.
- *
- * @covers \OpenEuropa\EuropaSearchClient\Model\Metadata
+ * @coversDefaultClass  \OpenEuropa\EuropaSearchClient\Model\Metadata
  */
 class MetadataTest extends TestCase
 {
     /**
-     * Tests the setters and getters.
+     * @covers ::setMetadata
+     * @covers ::getMetadata
      */
     public function testSettersAndGetters(): void
     {
         $model = new Metadata();
-        $model->setCollection([
+        $model->setMetadata([
             'BODY_VALUE' => ["Value 1", "Value 2"],
         ]);
-        $this->assertEquals($model->getCollection(), ['BODY_VALUE' => ["Value 1", "Value 2"]]);
+        $this->assertSame(['BODY_VALUE' => ['Value 1', 'Value 2']], $model->getMetadata());
+        // Same but with array access.
+        $this->assertSame(['Value 1', 'Value 2'], $model['BODY_VALUE']);
 
-        $model->setCollection([
-            'BODY_VALUE' => ["Value 1", "Value 2"],
-            'TITLE' => ["Value 1"],
+        $model->setMetadata([
+            'BODY_VALUE' => ['Value 1', 'Value 2'],
+            'TITLE' => ['Value 1'],
         ]);
-        $this->assertEquals($model->getCollection(), [
-            'BODY_VALUE' => ["Value 1", "Value 2"],
-            'TITLE' => ["Value 1"],
-        ]);
+        $this->assertSame([
+            'BODY_VALUE' => ['Value 1', 'Value 2'],
+            'TITLE' => ['Value 1'],
+        ], $model->getMetadata());
+        // Same but with array access.
+        $this->assertSame(['Value 1', 'Value 2'], $model['BODY_VALUE']);
+        $this->assertSame(['Value 1'], $model['TITLE']);
     }
 
     /**
-     * Tests JSON serialization.
+     * @covers ::jsonSerialize
      */
     public function testSerialization(): void
     {
         $model = new Metadata();
-        $model->setCollection([
+        $model->setMetadata([
             'BODY_VALUE' => ["Value 1", "Value 2"],
         ]);
         $actual = json_encode($model);
-        $this->assertEquals('{"BODY_VALUE":["Value 1","Value 2"]}', $actual);
+        $this->assertSame('{"BODY_VALUE":["Value 1","Value 2"]}', $actual);
+    }
+
+    /**
+     * @covers ::validateOffset
+     * @covers ::validateValue
+     *
+     * @dataProvider providerInvalidOffsetOrValue
+     *
+     * @param array $metadata
+     * @param string $expectedExceptionMessage
+     */
+    public function testInvalidOffsetOrValue(array $metadata, string $expectedExceptionMessage): void
+    {
+        $model = new Metadata();
+        $this->expectExceptionObject(new \InvalidArgumentException($expectedExceptionMessage));
+        $model->setMetadata($metadata);
+    }
+
+    /**
+     * @see self::testInvalidOffsetOrValue()
+     * @return array[]
+     */
+    public function providerInvalidOffsetOrValue(): array
+    {
+        return [
+            'non-string offset' => [
+                [123 => []],
+                'Passed offset should be a non-empty string. Given: 123',
+            ],
+            'empty string offset' => [
+                ['' => []],
+                "Passed offset should be a non-empty string. Given: ''",
+            ],
+            'non-array value' => [
+                ['field1' => new \stdClass()],
+                "The metadata 'field1' value should be an array. Given: (object) array(\n)",
+            ],
+            'array value with non-scalar' => [
+                ['field1' => [new \stdClass()]],
+                "The metadata 'field1' value, offset '0', should be a scalar. Given: (object) array(\n)",
+            ],
+        ];
     }
 }
