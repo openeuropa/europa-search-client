5b156dd716655d09114e8135f62928829ff2e571
Vincent Timperio <vincent.timperio@gmail.com>
2021-07-12 12:09:02 +0200

    OEL-291: Class name changed from EuropaSearchApiInvalidParameterValueException to ParameterValueException.

--- src/Exception/EuropaSearchApiInvalidParameterValueException.php
+++ src/Exception/EuropaSearchApiInvalidParameterValueException.php
@@ -7,7 +7,7 @@ namespace OpenEuropa\EuropaSearchClient\Exception;
 /**
  * Thrown when a parameter has a invalid value.
  */
-class EuropaSearchApiInvalidParameterValueException extends \InvalidArgumentException
+class ParameterValueException extends \InvalidArgumentException
 {
 
 }
